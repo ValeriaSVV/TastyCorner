fc5abf85a5f5d267e2b2562266615488de032a14
Валерия Санджиева <lerasandzieva@gmail.com>
2023-01-19 08:46:09 +0300

    Добавьте файлы проекта.

---
+++ TastyCorner/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TastyCorner
+{
+   
+        /// <summary>
+        /// Interaction logic for LoginWindow.xaml
+        /// </summary>
+        public partial class MainWindow : Window
+        {
+            public static bool user;
+
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+
+          
+
+            private void CheckBox_Click(object sender, RoutedEventArgs e)
+            {
+                if (chkShow.IsChecked.Value)
+                {
+                    txtPassword.Text = pswPassword.Password;
+                    txtPassword.Visibility = Visibility.Visible;
+                    pswPassword.Visibility = Visibility.Hidden;
+                }
+                else
+                {
+                    pswPassword.Password = txtPassword.Text;
+                    pswPassword.Visibility = Visibility.Visible;
+                    txtPassword.Visibility = Visibility.Hidden;
+                }
+            }
+
+            private void btnExit_Click(object sender, RoutedEventArgs e)
+            {
+                Application.Current.Shutdown();
+            }
+
+        private void btnOrder_Click(object sender, RoutedEventArgs e)
+        {
+          
+                Order order = new Order();
+            order.Show();
+            Close();
+
+                if (txtLogin.Text == "" || pswPassword.Password == "")
+                    MessageBox.Show("Не хватает данных");
+                else
+                {
+                    if (txtLogin.Text == "admin" && pswPassword.Password == "admin")
+                    {
+                        user = true;
+                        order.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        if (txtLogin.Text == "client" && pswPassword.Password == "client")
+                        {
+                            user = false;
+                            order.Show();
+                            Close();
+                        }
+                        else
+                            MessageBox.Show("Проверьте логин или пароль");
+                    }
+                }
+            }
+        }
+    }
+    
---
+++ TastyCorner/Order.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TastyCorner
+{
+    /// <summary>
+    /// Логика взаимодействия для Order.xaml
+    /// </summary>
+    public partial class Order : Window
+    {
+
+        MainWindow mainWindow = new MainWindow();
+
+        public Order()
+        {
+            InitializeComponent();
+            
+        }
+
+
+            private void Window_Loaded(object sender, RoutedEventArgs e)
+            {
+
+                if (MainWindow.user == true)
+                {
+                    btnOrder.Visibility = Visibility.Hidden;
+                    chkMash.IsEnabled = false;
+                    chkTiramisu.IsEnabled = false;
+                    chkSoup.IsEnabled = false;
+                    lblUser.Content = "Администратор";
+                }
+                else
+                {
+                    txtMash.IsEnabled = false;
+                    txtTiramisu.IsEnabled = false;
+                    txtSoup.IsEnabled = false;
+                    lblUser.Content = "Клиент";
+                }
+            }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+   
+    }
+    }
+
